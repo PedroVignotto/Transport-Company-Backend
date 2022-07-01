ce1b6b496798dc9d181a227f139ab0a958bf9900
PedroVignotto <pedro.vignotto@hotmail.com>
2022-07-01 15:37:53 -0300

    refactor: remove unused code

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -2,17 +2,8 @@ import { ServerError } from '@/application/errors'
 
 export type HttpResponse<T = any> = { statusCode: number, data: T }
 
-export const ok = <T = any> (data: T): HttpResponse<T> => ({
-  statusCode: 200,
-  data
-})
+export const ok = <T = any> (data: T): HttpResponse<T> => ({ statusCode: 200, data })
 
-export const badRequest = (error: Error): HttpResponse<Error> => ({
-  statusCode: 400,
-  data: error
-})
+export const badRequest = (error: Error): HttpResponse<Error> => ({ statusCode: 400, data: error })
 
-export const serverError = (error: unknown): HttpResponse<Error> => ({
-  statusCode: 500,
-  data: new ServerError(error instanceof Error ? error : undefined)
-})
+export const serverError = (error: Error): HttpResponse<Error> => ({ statusCode: 500, data: new ServerError(error) })
