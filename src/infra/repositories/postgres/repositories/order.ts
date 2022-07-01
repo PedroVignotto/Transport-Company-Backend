7b4829677106721b192a0fa34e34f82d8218eb98
PedroVignotto <pedro.vignotto@hotmail.com>
2022-07-01 14:18:30 -0300

    feat: ensure OrderRepository return order data if already exists

--- src/infra/repositories/postgres/repositories/order.ts
+++ src/infra/repositories/postgres/repositories/order.ts
@@ -5,6 +5,6 @@ import { LoadOrderByTrackingCodeRepository } from '@/domain/contracts/database/r
 export class OrderRepository extends PgRepository implements LoadOrderByTrackingCodeRepository {
   async loadByTrackingCode ({ trackingCode }: LoadOrderByTrackingCodeRepository.Input): Promise<LoadOrderByTrackingCodeRepository.Output> {
     const repository = this.getRepository(Order)
-    return await repository.findOne({ trackingCode })
+    return await repository.findOne({ where: { trackingCode }, relations: ['deliveryStatus'] })
   }
 }
