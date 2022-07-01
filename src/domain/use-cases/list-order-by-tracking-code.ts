5eddea6de78bdae9fa7fce6d07394cf3196fec7f
PedroVignotto <pedro.vignotto@hotmail.com>
2022-07-01 11:48:44 -0300

    feat: ensure ListOrderByTrackingCode return a order data on success

--- src/domain/use-cases/list-order-by-tracking-code.ts
+++ src/domain/use-cases/list-order-by-tracking-code.ts
@@ -1,12 +1,14 @@
 import { LoadOrderByTrackingCodeRepository } from '@/domain/contracts/database/repositories'
 import { FieldNotFoundError } from '@/domain/errors'
+import { Order } from '@/domain/models'
 
 type Setup = (orderRepository: LoadOrderByTrackingCodeRepository) => ListOrderByTrackingCode
 type Input = { trackingCode: string }
-type Output = void
+type Output = Order
 export type ListOrderByTrackingCode = (input: Input) => Promise<Output>
 
 export const listOrderByTrackingCodeUseCase: Setup = orderRepository => async ({ trackingCode }) => {
   const order = await orderRepository.loadByTrackingCode({ trackingCode })
   if (!order) throw new FieldNotFoundError('trackingCode')
+  return order
 }
