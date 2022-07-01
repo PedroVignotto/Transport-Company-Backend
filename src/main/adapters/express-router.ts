667ac20043bd665f7778fc97eca77850f0eabe05
PedroVignotto <pedro.vignotto@hotmail.com>
2022-07-01 16:42:12 -0300

    feat: ensure expressRouterAdapter respond with correct statusCode and data on success

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -5,5 +5,6 @@ import { RequestHandler } from 'express'
 type Adapter = (controller: Controller) => RequestHandler
 
 export const expressRouterAdapter: Adapter = controller => async (req, res) => {
-  await controller.handle({ ...req.params })
+  const { statusCode, data } = await controller.handle({ ...req.params })
+  res.status(statusCode).json(data)
 }
