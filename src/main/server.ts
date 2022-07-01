caf559816e37ff276770d8c19d66bd7f81443f19
PedroVignotto <pedro.vignotto@hotmail.com>
2022-07-01 18:30:53 -0300

    chore: add .env config

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,8 +2,9 @@ import './config/module-alias'
 import { PgConnection } from '@/infra/database/postgres/helpers'
 
 import 'reflect-metadata'
+import 'dotenv/config'
 
 PgConnection.getInstance().connect().then(async () => {
   const { app } = await import('@/main/config/app')
-  app.listen(3333, () => console.log('Server running at http://localhost:3333'))
+  app.listen(process.env.PORT, () => console.log(`Server running at http://localhost:${process.env.PORT!}`))
 }).catch(console.error)
