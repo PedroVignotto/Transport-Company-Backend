fe75921a3ffa5c1d92db3ab299d67e615a38caeb
PedroVignotto <pedro.vignotto@hotmail.com>
2022-07-01 15:24:15 -0300

    feat: ensure ListOrderByTrackingCodeController return 200 with order data on success

--- src/application/controllers/list-order-by-tracking-code.ts
+++ src/application/controllers/list-order-by-tracking-code.ts
@@ -1,17 +1,18 @@
 import { ok, HttpResponse, badRequest, serverError } from '@/application/helpers'
-import { FieldNotFoundError } from '@/domain/errors'
 import { ListOrderByTrackingCode } from '@/domain/use-cases'
+import { FieldNotFoundError } from '@/domain/errors'
+import { Order } from '@/domain/models'
 
 type HttpRequest = { trackingCode: string }
-type Model = undefined | Error
+type Model = Order | Error
 
 export class ListOrderByTrackingCodeController {
   constructor (private readonly listOrderByTrackingCode: ListOrderByTrackingCode) {}
 
   async handle ({ trackingCode }: HttpRequest): Promise<HttpResponse<Model>> {
     try {
-      await this.listOrderByTrackingCode({ trackingCode })
-      return ok(undefined)
+      const order = await this.listOrderByTrackingCode({ trackingCode })
+      return ok(order)
     } catch (error: any) {
       if (error instanceof FieldNotFoundError) return badRequest(error)
       return serverError(error)
