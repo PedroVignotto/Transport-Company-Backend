eecb1e5cd884511eb099bcb34c3c86c67421cf54
PedroVignotto <pedro.vignotto@hotmail.com>
2022-07-01 14:18:46 -0300

    test: ensure OrderRepository return order data if already exists

--- tests/infra/repositories/postgres/repositories/order.spec.ts
+++ tests/infra/repositories/postgres/repositories/order.spec.ts
@@ -1,20 +1,27 @@
 import { generateRandomOrder } from '@/tests/mocks'
 import { makeFakeDatabase } from '@/tests/infra/repositories/postgres/mocks'
 import { OrderRepository } from '@/infra/repositories/postgres/repositories'
+import { DeliveryStatus, Order } from '@/infra/repositories/postgres/entities'
+import { PgConnection } from '@/infra/repositories/postgres/helpers'
 
 import { IBackup, IMemoryDb } from 'pg-mem'
+import { Repository } from 'typeorm'
 
 describe('OrderRepository', () => {
   let sut: OrderRepository
-  let trackingCode: string
+  let order: Order
   let backup: IBackup
   let database: IMemoryDb
+  let orderRepository: Repository<Order>
+  let deliveryStatusRepository: Repository<DeliveryStatus>
 
   beforeAll(async () => {
-    trackingCode = generateRandomOrder().trackingCode
+    order = generateRandomOrder()
 
     database = await makeFakeDatabase()
     backup = database.backup()
+    orderRepository = PgConnection.getInstance().getRepository(Order)
+    deliveryStatusRepository = PgConnection.getInstance().getRepository(DeliveryStatus)
   })
 
   beforeEach(() => {
@@ -24,8 +31,17 @@ describe('OrderRepository', () => {
   })
 
   it('Should return undefined if order does not exists', async () => {
-    const order = await sut.loadByTrackingCode({ trackingCode })
+    const result = await sut.loadByTrackingCode({ trackingCode: order.trackingCode })
 
-    expect(order).toBeUndefined()
+    expect(result).toBeUndefined()
+  })
+
+  it('Should return order data if already exists', async () => {
+    await deliveryStatusRepository.save(order.deliveryStatus)
+    const createdOrder = await orderRepository.save(order)
+
+    const result = await sut.loadByTrackingCode({ trackingCode: order.trackingCode })
+
+    expect(result).toEqual(createdOrder)
   })
 })
